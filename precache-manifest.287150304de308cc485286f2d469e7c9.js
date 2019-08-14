11111afb0cdfc38d43ebd092b17b79a6883dc8c9
dungeoneers-io <yusif.elawawdeh@mymail.aud.edu>
2019-08-13 21:40:53 -0500

    Updates

---
+++ precache-manifest.287150304de308cc485286f2d469e7c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146ce18cf4f729ce6a34387f25440554",
+    "url": "/dungeoneers/index.html"
+  },
+  {
+    "revision": "b11ced9e2e0a32e926b4",
+    "url": "/dungeoneers/static/css/main.b8ebcc74.chunk.css"
+  },
+  {
+    "revision": "924fd3d20abcf542bee4",
+    "url": "/dungeoneers/static/js/2.e22bb064.chunk.js"
+  },
+  {
+    "revision": "b11ced9e2e0a32e926b4",
+    "url": "/dungeoneers/static/js/main.9f51bea8.chunk.js"
+  },
+  {
+    "revision": "00d1c34d4476ce1701eb",
+    "url": "/dungeoneers/static/js/runtime~main.f11a8c82.js"
+  },
+  {
+    "revision": "360ae478f4b84a340b9e7f1ce0bc2f95",
+    "url": "/dungeoneers/static/media/dungeoneers_torch.360ae478.gif"
+  }
+]);
